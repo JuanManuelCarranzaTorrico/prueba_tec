bc6096289a0e910de14b7286a8241a07337a31c9
Juan Manuel Carranza <jcarranza@bancosol.com.bo>
2024-11-06 00:13:47 -0400

    new post screen added

--- lib/presentation/screens/post/new_post.dart
+++ lib/presentation/screens/post/new_post.dart
@@ -13,7 +13,58 @@ class NewPost extends StatelessWidget {
         foregroundColor: Colors.white,
         centerTitle: true,
       ),
-      body: Placeholder()
+      body: _postForm(),
+    );
+  }
+}
+//statefull widget with a form to create a new post
+class _postForm extends StatefulWidget {
+  const _postForm({super.key});
+
+  @override
+  State<_postForm> createState() => _postFormState();
+}
+
+class _postFormState extends State<_postForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        children: [
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Title',
+            ),
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Description',
+            ),
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'UserId',
+            ),
+          ),
+          //button on the bottom of the screen
+          const Spacer(),
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {}, 
+              style: ElevatedButton.styleFrom(
+                primary: const Color.fromARGB(255, 1, 95, 172),
+                onPrimary: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.zero,
+                )
+              ),
+              child: const Text('GUARDAR'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
